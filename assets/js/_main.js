ca5f05bd4306774cb7e48d916114561cac16fbe4
yangyukun02 <yangyukun02@meituan.com>
2018-08-12 10:44:49 +0800

    fix

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -71,7 +71,7 @@ $(document).ready(function() {
     return false;
   });
 
-  $("img").addClass("image-popup");
+  $("article img").addClass("image-popup");
   // add lightbox class to all image links
   $(
     "a[href$='.jpg'],a[href$='.jpeg'],a[href$='.JPG'],a[href$='.png'],a[href$='.gif']"
@@ -79,36 +79,25 @@ $(document).ready(function() {
 
   // Magnific-Popup options
   $(".image-popup").magnificPopup({
-    // disableOn: function() {
-    //   if( $(window).width() < 500 ) {
-    //     return false;
-    //   }
-    //   return true;
-    // },
-    type: "image",
-    tLoading: "Loading image #%curr%...",
-    gallery: {
-      enabled: true,
-      navigateByImgClick: true,
-      preload: [0, 1] // Will preload 0 - before current, and 1 after the current image
-    },
+    type: 'image',
+    closeOnContentClick: false,
+    closeBtnInside: false,
+    mainClass: 'mfp-with-zoom mfp-img-mobile',
     image: {
-      tError: '<a href="%url%">Image #%curr%</a> could not be loaded.'
-    },
-    removalDelay: 100, // Delay in milliseconds before popup is removed
-    // Class that is added to body when popup is open.
-    // make it unique to apply your CSS animations just to this exact popup
-    mainClass: "mfp-zoom-in",
-    callbacks: {
-      beforeOpen: function() {
-        // just a hack that adds mfp-anim class to markup
-        this.st.image.markup = this.st.image.markup.replace(
-          "mfp-figure",
-          "mfp-figure mfp-with-anim"
-        );
+      verticalFit: true,
+      titleSrc: function (item) {
+        return item.el.attr('title');
       }
     },
-    closeOnContentClick: true,
-    midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
+    gallery: {
+      enabled: true
+    },
+    zoom: {
+      enabled: true,
+      duration: 300, // don't foget to change the duration also in CSS
+      opener: function (element) {
+        return element.find('img');
+      }
+    }
   });
 });
